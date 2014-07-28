48c4ec3bfe8abcc65630ae1961d6985a532c79f5
Mikael Jergefelt <mike@redvolume.com>
2014-07-28 20:54:39 +0200

    Removed spaces

--- js/rps.js
+++ js/rps.js
@@ -168,9 +168,9 @@ var rps = {
 
 Array.prototype.shuffle = function() {
   var i = this.length, j, temp;
-  if ( i == 0 ) return this;
-  while ( --i ) {
-     j = Math.floor( Math.random() * ( i + 1 ) );
+  if (i == 0) return this;
+  while (--i) {
+     j = Math.floor(Math.random() * (i + 1));
      temp = this[i];
      this[i] = this[j];
      this[j] = temp;
